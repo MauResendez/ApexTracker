c39850736bd73bda338927c6a42d0a1b5fe5fab0
Mauricio <mauricioresendez524@gmail.com>
2021-02-24 02:18:27 -0600

    Created the back end with API

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+// Load env
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+// Dev logging
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'));
+}
+
+// Routes
+app.use('/api/v2/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(3000, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
+
+
