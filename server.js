7593118cd6e8c595234327daae1d72e813839497
Mauricio <mauricioresendez524@gmail.com>
2021-02-28 14:22:38 -0600

    Added build folder to root for deployment

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const path = require('path');
 
 // Load env
 dotenv.config({ path: './config.env' });
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Dev logging
 if(process.env.NODE_ENV === 'development')
@@ -13,10 +16,15 @@ if(process.env.NODE_ENV === 'development')
     app.use(morgan('dev'));
 }
 
+
 // Routes
 app.use('/api/v2/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 5000;
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(5000, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
